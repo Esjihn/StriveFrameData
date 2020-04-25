b2e77ecf0e5d9ed5768b409304ffcddf37528b40
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-04-24 21:12:55 -0400

    Fixed UserControl load in tabControl

--- StriveFrameData/Views/MainFrameDataView.Designer.cs
+++ StriveFrameData/Views/MainFrameDataView.Designer.cs
@@ -55,19 +55,19 @@
             this.tabCharacters.Name = "tabCharacters";
             this.tabCharacters.Padding = new System.Drawing.Point(10, 15);
             this.tabCharacters.SelectedIndex = 0;
-            this.tabCharacters.Size = new System.Drawing.Size(1310, 829);
+            this.tabCharacters.Size = new System.Drawing.Size(1415, 905);
             this.tabCharacters.TabIndex = 0;
             // 
             // tabSolPage
             // 
+            this.tabSolPage.AutoScroll = true;
             this.tabSolPage.BackColor = System.Drawing.Color.DarkRed;
             this.tabSolPage.Location = new System.Drawing.Point(4, 79);
             this.tabSolPage.Name = "tabSolPage";
             this.tabSolPage.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSolPage.Size = new System.Drawing.Size(1302, 746);
+            this.tabSolPage.Size = new System.Drawing.Size(1407, 822);
             this.tabSolPage.TabIndex = 1;
             this.tabSolPage.Text = "Sol";
-            this.tabSolPage.Click += new System.EventHandler(this.tabSolPage_Click);
             // 
             // tabKyPage
             // 
@@ -133,7 +133,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1310, 829);
+            this.ClientSize = new System.Drawing.Size(1415, 905);
             this.Controls.Add(this.tabCharacters);
             this.Name = "MainFrameDataView";
             this.Text = "Guilty Gear Strive Frame Data";
--- StriveFrameData/Views/MainFrameDataView.cs
+++ StriveFrameData/Views/MainFrameDataView.cs
@@ -9,12 +9,35 @@ namespace StriveFrameData.Views
         public MainFrameDataView()
         {
             InitializeComponent();
+            AddFrameDataControlToTabs();
         }
 
-        private void tabSolPage_Click(object sender, System.EventArgs e)
+        #region Private Methods
+
+        /// <summary>
+        /// Load main frame data user control into all character tabs
+        /// </summary>
+        private void AddFrameDataControlToTabs()
         {
             FrameDataControl fdc = new FrameDataControl();
-            this.tabSolPage.Controls.Add(fdc);
+            fdc.MaximumSize = new Size(1302, 703);
+            FrameDataControl fdc2 = new FrameDataControl();
+            FrameDataControl fdc3 = new FrameDataControl();
+            FrameDataControl fdc4 = new FrameDataControl();
+            FrameDataControl fdc5 = new FrameDataControl();
+            FrameDataControl fdc6 = new FrameDataControl();
+            FrameDataControl fdc7 = new FrameDataControl();
+
+            this.tabMayPage.Controls.Add(fdc);
+            this.tabSolPage.Controls.Add(fdc2);
+            this.tabKyPage.Controls.Add(fdc3);
+            this.tabAxlPage.Controls.Add(fdc4);
+            this.tabChippPage.Controls.Add(fdc5);
+            this.tabFaustPage.Controls.Add(fdc6);
+            this.tabPotemkinPage.Controls.Add(fdc7);
+            this.Dock = DockStyle.Fill;
         }
+
+#endregion
     }
 }
